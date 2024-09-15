cd977a2131ca23df4dd2dd6639120198f53c01dd
Kalcha <diego.rbarrionuevo@gmail.com>
2024-09-15 12:19:54 -0300

    react3

--- react/components/router/AppRouter.tsx
+++ react/components/router/AppRouter.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react"
 import {Routes, Route} from "react-router-dom"
 import Create from "../../pages/Create"
 import Home from "../../pages/Home"
 import Feed from "../../pages/Feed"
 
+//http://cloninstagram.com/
+//http://cloninstagram.com/feed
+//http://cloninstagram.com/create
 const AppRoutes: React.FC = () => {
     return (
         <Routes>
-            <Route path="/" element={} />
-            <Route path="/feed" element={} />
-            <Route path="/create" element={} />
+            <Route path="/" element={<Home />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/create" element={<Create />} />
         </Routes>
     )
 }
+
+export default AppRoutes
